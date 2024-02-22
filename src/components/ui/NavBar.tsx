89e49436d81f3e861a41f59236670bdbb2cd01c0
iamgodot <xugodot@gmail.com>
2024-02-21 17:23:58 -0800

    Add user profile picture

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -1,15 +1,15 @@
-"use client";
+"use client"
 
-import MaxWidthWrapper from "./MaxWidthWrapper";
-import Link from "next/link";
-import { buttonVariants } from "./button";
-import { ArrowRight } from "lucide-react";
-import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
-import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
-import Image from "next/image";
+import MaxWidthWrapper from "./MaxWidthWrapper"
+import Link from "next/link"
+import { buttonVariants } from "./button"
+import { ArrowRight } from "lucide-react"
+import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs"
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs"
+import Image from "next/image"
 
 function NavBar() {
-    const { user } = useKindeBrowserClient();
+    const { user } = useKindeBrowserClient()
 
     return (
         <nav className="sticky h-12 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
@@ -23,34 +23,55 @@ function NavBar() {
                             <>
                                 <Link
                                     href="/pricing"
-                                    className={buttonVariants({ variant: "ghost", size: "sm" })}
+                                    className={buttonVariants({
+                                        variant: "ghost",
+                                        size: "sm",
+                                    })}
                                 >
                                     Pricing
                                 </Link>
-                                <LoginLink className={buttonVariants({ size: "sm" })}>
-                                    Sign in <ArrowRight className="ml-1.5 h-5 w-5" />
+                                <LoginLink
+                                    className={buttonVariants({ size: "sm" })}
+                                >
+                                    Sign in{" "}
+                                    <ArrowRight className="ml-1.5 h-5 w-5" />
                                 </LoginLink>
                             </>
                         ) : (
                             <>
                                 <Link
                                     href="/dashboard"
-                                    className={buttonVariants({ variant: "ghost", size: "sm" })}
+                                    className={buttonVariants({
+                                        variant: "ghost",
+                                        size: "sm",
+                                    })}
                                 >
                                     Dashboard
                                 </Link>
                                 <LogoutLink
-                                    className={buttonVariants({ variant: "ghost", size: "sm" })}
+                                    className={buttonVariants({
+                                        variant: "ghost",
+                                        size: "sm",
+                                    })}
                                 >
                                     Sign out
                                 </LogoutLink>
+                                {user && (
+                                    <Image
+                                        src={user.picture}
+                                        alt="user picture"
+                                        width={48}
+                                        height={48}
+                                        className="rounded-md"
+                                    />
+                                )}
                             </>
                         )}
                     </div>
                 </div>
             </MaxWidthWrapper>
         </nav>
-    );
+    )
 }
 
-export default NavBar;
+export default NavBar
