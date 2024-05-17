966923b88cfb7416a81af974f8673c3b4f1378b2
iamgodot <xugodot@gmail.com>
2024-05-16 23:56:30 -0700

    Refine PDF renderer

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,39 +1,41 @@
 "use client"
 
 import React from "react"
-import {
-  PageLayout,
-  SpecialZoomLevel,
-  Viewer,
-  Worker,
-} from "@react-pdf-viewer/core"
-import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout"
+import { Viewer, Worker } from "@react-pdf-viewer/core"
 import "@react-pdf-viewer/core/lib/styles/index.css"
 import "@react-pdf-viewer/default-layout/lib/styles/index.css"
-import { useResizeDetector } from "react-resize-detector"
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout"
+import type {
+  ToolbarProps,
+  ToolbarSlot,
+  TransformToolbarSlot,
+} from "@react-pdf-viewer/toolbar"
 
 interface PdfRendererProps {
   url: string
 }
 
 function PdfRenderer({ url }: PdfRendererProps) {
-  const defaultLayoutPluginInstance = defaultLayoutPlugin()
-  const { width, height, ref } = useResizeDetector()
-  const pageLayout: PageLayout = {}
+  const transform: TransformToolbarSlot = (slot: ToolbarSlot) => ({
+    ...slot,
+    ShowSearchPopover: () => <></>,
+    Open: () => <></>,
+    Print: () => <></>,
+    MoreActionsPopover: () => <></>,
+  })
+  const renderToolbar = (
+    Toolbar: (props: ToolbarProps) => React.ReactElement
+  ) => <Toolbar>{renderDefaultToolbar(transform)}</Toolbar>
+  const defaultLayoutPluginInstance = defaultLayoutPlugin({
+    renderToolbar,
+  })
+  const { renderDefaultToolbar } =
+    defaultLayoutPluginInstance.toolbarPluginInstance
   return (
-    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
-      <div ref={ref} className="flex-1 w-full max-h-screen">
-        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.js">
-          <div
-            style={{
-              border: "1px solid rgba(0, 0, 0, 0.3)",
-              height: "750px",
-            }}
-          >
-            <Viewer fileUrl={url} plugins={[defaultLayoutPluginInstance]} />
-          </div>
-        </Worker>
-      </div>
+    <div className="w-full h-screen p-5 shadow lg:h-full">
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.js">
+        <Viewer fileUrl={url} plugins={[defaultLayoutPluginInstance]} />
+      </Worker>
     </div>
   )
 }
