ce5c9ad7db32aa2ebb51de0cc5a59330849384c5
iamgodot <xugodot@gmail.com>
2024-06-03 18:05:21 -0700

    refactor: make loading component more visible

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,10 +3,10 @@ import { Skeleton } from "@/components/ui/skeleton"
 export default function Loading() {
   return (
     <div className="flex flex-col items-center pt-24 space-y-3">
-      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+      <Skeleton className="h-[125px] w-[250px] rounded-xl bg-zinc-300" />
       <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+        <Skeleton className="h-4 w-[250px] bg-zinc-300" />
+        <Skeleton className="h-4 w-[200px] bg-zinc-300" />
       </div>
     </div>
   )
